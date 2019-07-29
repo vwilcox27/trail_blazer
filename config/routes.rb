5302f0795c15b0471bc0bd3715cb68040d48c7fa
Vance Wilcox <peter.wilcox@acimacredit.com>
2019-07-29 17:01:50 -0600

    added a User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'trails#index'
   get '/about',   to: 'trails#about'
   get '/help',    to: 'trails#help'
   get '/contact', to: 'trails#contact'
-
+  get '/signup',  to: 'users#new'
 end
