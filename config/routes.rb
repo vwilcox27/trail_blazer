820745ec61cb2c1811beef6c2e8b0d96ce70f627
Vance Wilcox <peter.wilcox@acimacredit.com>
2019-08-02 14:35:26 -0600

    began adding sign up page styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   root 'trails#index'
   get '/about',   to: 'trails#about'
   get '/help',    to: 'trails#help'
   get '/contact', to: 'trails#contact'
   get '/signup',  to: 'users#new'
+  resources :users
 end
